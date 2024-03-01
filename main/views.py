687f7bed218b3c4aaf8a29fcbeed55378f9e9f2d
DavlatovO <davlatovoybek007@gmail.com>
2024-03-02 00:56:57 +0500

    first commit

---
+++ main/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from . import models
+
+def index(request):
+    about_me = models.AboutMe.objects.last()
+    educations = models.Education.objects.all()
+    skills = models.Skill.objects.all()
+    experiences = models.Experience.objects.all()
+    profiles = models.Profiles.objects.all()
+    portfolio_items = models.Portfolio.objects.all()
+    clients = models.Client.objects.all()
+    context = {
+        'about_me':about_me,
+        'educations':educations,
+        'skills':skills,
+        'experiences':experiences,
+        'profile':profiles,
+        'portfolio_items': portfolio_items,
+        'clients': clients
+    }
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        models.Contact.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message
+        )
+     
+        
+        # contact = models.Contact(email=email, name=name, subject=subject, message=message)
+        # contact.save()
+    return render(request, 'index.html', context)
+
